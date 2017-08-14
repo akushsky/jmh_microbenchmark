29e370739e7a2d6ad1741f37f22ea91a75028e06
Namelles.One <namelles.one@gmail.com>
2017-08-14 13:39:35 +0300

    Update JMH version, change to 10 threads

--- src/main/java/com/amdocs/ch/JsonBenchmark.java
+++ src/main/java/com/amdocs/ch/JsonBenchmark.java
@@ -28,15 +28,16 @@ package com.amdocs.ch;
 import com.amdocs.ch.events.NotificationEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jsoniter.JsonIterator;
-import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
+import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
 
 import java.io.IOException;
 
 public class JsonBenchmark {
 
-	@State(Scope.Thread)
+	@State(Scope.Benchmark)
 	public static class MyState {
 		String message = "{\"properties\":{\"ch.security\":\"true\",\"state.event.device.uid\":\"hdm:tyco:TYCO_SECURITY_PANEL/1\",\"state.event.property.value\":\"true\",\"locationIdsRequest\":\"true\",\"device.timestamp.added\":\"1463402030916\",\"deviceClasses\":\"com.prosyst.mbs.services.hdm.deviceclasses.BinarySensor,com.prosyst.mbs.services.hdm.deviceclasses.Key\",\"eventProtocol\":\"Simulator\",\"state.event.property.name\":\"sendAllPropertiesStateEvent\",\"isParent\":\"true\",\"eventSource\":\"syncHdmDevice\",\"sendAllPropertiesStateEvent\":\"true\",\"type\":\"tyco_powermaster360_security_panel\",\"device.firmware.version\":\"P-Max: \\\"JS703296 L19.412\\\", P-Default: \\\"J-703302 L19.412\\\", P-Link: \\\"8.0.92.3\\\"-\\\"raw\\\", EEPROM: 13.150\",\"device.external.serial\":\"13DFE5\"},\"gateway\":\"1116308256\",\"timestamp\":1502690866119,\"device\":\"hdm:tyco:TYCO_SECURITY_PANEL/1\",\"vendor\":\"PROSYST\",\"type\":\"DEVICE_STATE_CHANGED\",\"severity\":\"INFO\", \"gateway\":\"1116308256\"}";
 
@@ -44,16 +45,17 @@ public class JsonBenchmark {
 		ObjectMapper jsonObjectMapper = new ObjectMapper();
 	}
 
-	@GenerateMicroBenchmark
-	public void jsoniterTest(MyState state) {
+	@Benchmark
+	@Threads(10)
+	public NotificationEvent jsoniterTest(MyState state) {
 		// place your benchmarked code here
-		NotificationEvent event = JsonIterator.deserialize(state.message, NotificationEvent.class);
+		return JsonIterator.deserialize(state.message, NotificationEvent.class);
 	}
 
-	@GenerateMicroBenchmark
-	public void jacksonTest(MyState state) throws IOException {
+	@Benchmark
+	@Threads(10)
+	public NotificationEvent jacksonTest(MyState state) throws IOException {
 		// place your benchmarked code here
-		NotificationEvent event = state.jsonObjectMapper.readValue(state.message, NotificationEvent.class);
+		return state.jsonObjectMapper.readValue(state.message, NotificationEvent.class);
 	}
-
 }
