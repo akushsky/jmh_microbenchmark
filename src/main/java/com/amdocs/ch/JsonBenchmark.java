93ccb15cfc66ef7d6b21b23a6e4d6a2d3ae57f71
Namelles.One <namelles.one@gmail.com>
2017-08-15 11:16:23 +0300

    Add both tests - strings and bytes. Add Jackson Afterburner tests.

--- src/main/java/com/amdocs/ch/JsonBenchmark.java
+++ src/main/java/com/amdocs/ch/JsonBenchmark.java
@@ -27,6 +27,7 @@ package com.amdocs.ch;
 
 import com.amdocs.ch.events.NotificationEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 import com.jsoniter.JsonIterator;
 import org.openjdk.jmh.annotations.*;
 
@@ -44,19 +45,46 @@ public class JsonBenchmark {
 	public static class MyState {
 		String message = "{\"properties\":{\"ch.security\":\"true\",\"state.event.device.uid\":\"hdm:tyco:TYCO_SECURITY_PANEL/1\",\"state.event.property.value\":\"true\",\"locationIdsRequest\":\"true\",\"device.timestamp.added\":\"1463402030916\",\"deviceClasses\":\"com.prosyst.mbs.services.hdm.deviceclasses.BinarySensor,com.prosyst.mbs.services.hdm.deviceclasses.Key\",\"eventProtocol\":\"Simulator\",\"state.event.property.name\":\"sendAllPropertiesStateEvent\",\"isParent\":\"true\",\"eventSource\":\"syncHdmDevice\",\"sendAllPropertiesStateEvent\":\"true\",\"type\":\"tyco_powermaster360_security_panel\",\"device.firmware.version\":\"P-Max: \\\"JS703296 L19.412\\\", P-Default: \\\"J-703302 L19.412\\\", P-Link: \\\"8.0.92.3\\\"-\\\"raw\\\", EEPROM: 13.150\",\"device.external.serial\":\"13DFE5\"},\"gateway\":\"1116308256\",\"timestamp\":1502690866119,\"device\":\"hdm:tyco:TYCO_SECURITY_PANEL/1\",\"vendor\":\"PROSYST\",\"type\":\"DEVICE_STATE_CHANGED\",\"severity\":\"INFO\", \"gateway\":\"1116308256\"}";
 
-		// POJO to JSON mapper
+		// JSON to POJO mapper
 		ObjectMapper jsonObjectMapper = new ObjectMapper();
+
+		// JSON to POJO mapper with afterburner module
+		ObjectMapper jsonObjectMapperAfterburner = (new ObjectMapper()).registerModule(new AfterburnerModule());
 	}
 
 	@Benchmark
-	public NotificationEvent jsoniterTest(MyState state) {
+	public NotificationEvent jsoniterTestString(MyState state) {
 		// place your benchmarked code here
 		return JsonIterator.deserialize(state.message, NotificationEvent.class);
 	}
 
 	@Benchmark
-	public NotificationEvent jacksonTest(MyState state) throws IOException {
+	public NotificationEvent jacksonTestString(MyState state) throws IOException {
 		// place your benchmarked code here
 		return state.jsonObjectMapper.readValue(state.message, NotificationEvent.class);
+	}
+
+	@Benchmark
+	public NotificationEvent jsoniterTestBytes(MyState state) {
+		// place your benchmarked code here
+		return JsonIterator.deserialize(state.message.getBytes(), NotificationEvent.class);
+	}
+
+	@Benchmark
+	public NotificationEvent jacksonTestBytes(MyState state) throws IOException {
+		// place your benchmarked code here
+		return state.jsonObjectMapper.readValue(state.message.getBytes(), NotificationEvent.class);
+	}
+
+	@Benchmark
+	public NotificationEvent jacksonTestString_Afterburner(MyState state) throws IOException {
+		// place your benchmarked code here
+		return state.jsonObjectMapperAfterburner.readValue(state.message, NotificationEvent.class);
+	}
+
+	@Benchmark
+	public NotificationEvent jacksonTestBytes_Afterburner(MyState state) throws IOException {
+		// place your benchmarked code here
+		return state.jsonObjectMapperAfterburner.readValue(state.message.getBytes(), NotificationEvent.class);
 	}
 }
